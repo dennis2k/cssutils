470333aa323110ddc82381c0cf8d4e8e09531d15
Dennis Mortensen <stinarn@gmail.com>
2017-11-08 10:59:35 +0100

    feat: added bundles with both media and no media queries

--- gulpfile.js
+++ gulpfile.js
@@ -8,16 +8,31 @@ var rename = require('gulp-rename');
 var concat = require('gulp-concat');
 var gzip = require("gulp-gzip");
 
-gulp.task('default', function () {
+gulp.task('all', function () {
     return gulp.src('./sass/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(concat('util.css'))
-        .pipe(gulp.dest('./'))
+        .pipe(gulp.dest('./dist'))
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./dist'))
         .pipe(gzip())
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest('./dist'));
 });
+
+gulp.task('no-media', function () {
+    return gulp.src(['./sass/*.scss', "!./sass/render-media.scss"])
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('util.nomedia.css'))
+        .pipe(gulp.dest('./dist'))
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./dist'))
+        .pipe(gzip())
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task("default",["all", "no-media"]);
 
 gulp.task("watch",() => {
     gulp.watch("./sass/*.scss", ['default']);
